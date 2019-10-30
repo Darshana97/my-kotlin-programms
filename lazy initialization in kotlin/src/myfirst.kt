8aaa30a4e8da0bb7efde00450f924039d5b15350
Darshana97 <dpbrayan1@gmail.com>
2019-10-30 23:06:22 +0530

    lazy initialization in kotlin

---
+++ lazy initialization in kotlin/src/myfirst.kt
@@ -0,0 +1,15 @@
+
+val pi: Float by lazy {
+    3.14f
+}
+
+fun main(args:Array<String>){
+
+    println("Some initial code....")
+
+    val area1 = pi * 4 * 4
+    val area2 = pi * 9 * 9
+
+    println("Some more code....")
+
+}
